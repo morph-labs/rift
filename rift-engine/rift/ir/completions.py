7ec205616115d118aa43be9977b267a882cfaabd
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-08-19 07:40:35 +0200

    Completions: add file path for each file, and range for each symbol.

--- rift-engine/rift/ir/completions.py
+++ rift-engine/rift/ir/completions.py
@@ -5,16 +5,25 @@ from typing import List
 import rift.ir.IR as IR
 
 @dataclass
-class SymbolInfo:
+class Symbol:
     name: str
     scope: str
     kind: str
+    range: IR.Range
+
+@dataclass
+class File:
+    path: str
+    symbols: List[Symbol]
 
 def get_symbol_completions(project: IR.Project) -> str:
-    symbol_infos: List[SymbolInfo] = []
-    for file in project.get_files():
-        for symbol in file.search_symbol(""):
-            symbol_info = SymbolInfo(symbol.name, symbol.scope, symbol.kind())
-            symbol_infos.append(symbol_info)
-    json_data = json.dumps([asdict(symbol) for symbol in symbol_infos], indent=4)
+    files: List[File] = []
+    for file_ir in project.get_files():
+        symbols: List[Symbol] = []
+        for symbol_info in file_ir.search_symbol(lambda _: True):
+            symbol= Symbol(symbol_info.name, symbol_info.scope, symbol_info.kind(), symbol_info.range)
+            symbols.append(symbol)
+        file = File(file_ir.path, symbols)
+        files.append(file)
+    json_data = json.dumps([asdict(symbol) for symbol in files], indent=4)
     return json_data
