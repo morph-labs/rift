a09cda74c58309d9be8c8c6b84f746083fed6395
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-09-12 22:11:02 +0200

    Remove rescript tree sitter support temporarily.
    Until the auto-installation issues are resolved.

--- rift-engine/rift/ir/IR.py
+++ rift-engine/rift/ir/IR.py
@@ -462,8 +462,8 @@ def language_from_file_extension(file_path: str) -> Optional[Language]:
         return "ocaml"
     elif file_path.endswith(".py"):
         return "python"
-    elif file_path.endswith(".res") and custom_parsers.active:
-        return "rescript"
+    # elif file_path.endswith(".res"):
+    #     return "rescript"
     elif file_path.endswith(".ts"):
         return "typescript"
     elif file_path.endswith(".tsx"):
--- rift-engine/rift/ir/custom_parsers.py
+++ rift-engine/rift/ir/custom_parsers.py
@@ -8,23 +8,10 @@ from rift.util.fs import RIFT_PROJECT_DIR
 TREE_SITTER_LANGUAGES_PATH = os.path.join(RIFT_PROJECT_DIR, "build", "tree-sitter-languages.so")
 VENDOR_PATH = os.path.join(RIFT_PROJECT_DIR, "vendor")
 
-active = False
-ReScript = None
+# Language.build_library(
+#     TREE_SITTER_LANGUAGES_PATH,
+#     [os.path.join(VENDOR_PATH, 'tree-sitter-rescript')])
 
-logger = logging.getLogger(__name__)
-
-def activate():
-    try:
-        global active, ReScript
-        Language.build_library(
-            TREE_SITTER_LANGUAGES_PATH, [os.path.join(VENDOR_PATH, "tree-sitter-rescript")]
-        )
-        ReScript = Language(TREE_SITTER_LANGUAGES_PATH, "rescript")
-        active = True
-    except Exception as e:
-        logger.error("Failed to activate custom parsers: %s", e)
-
-if active:
-    activate()
+# ReScript = Language(TREE_SITTER_LANGUAGES_PATH, 'rescript')
 
 parser: Parser = Parser()
