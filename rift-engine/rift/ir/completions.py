9075ac33e4cac2957dfffae0b149430d233c6e6a
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-09-18 12:05:19 +0200

    Begin parsing function bodies, and new symbol of kind IR.BlockKind.

--- rift-engine/rift/ir/completions.py
+++ rift-engine/rift/ir/completions.py
@@ -1,6 +1,6 @@
 import json
 from dataclasses import asdict, dataclass
-from typing import List
+from typing import Dict, List
 
 import rift.ir.IR as IR
 
@@ -23,11 +23,13 @@ def get_symbol_completions(project: IR.Project) -> str:
     return json.dumps(get_symbol_completions_raw(project), indent=4)
 
 
-def get_symbol_completions_raw(project: IR.Project) -> str:
+def get_symbol_completions_raw(project: IR.Project) -> List[Dict[str, Symbol]]:
     files: List[File] = []
     for file_ir in project.get_files():
         symbols: List[Symbol] = []
         for symbol_info in file_ir.search_symbol(lambda _: True):
+            if isinstance(symbol_info, IR.ValueDeclaration) and isinstance(symbol_info.value_kind, IR.BlockKind):
+                continue # don't emit completions for blocks at the moment
             symbol = Symbol(
                 symbol_info.name, symbol_info.scope, symbol_info.kind(), symbol_info.range
             )
